ab1e6678c947bfa71d9b7b353720b8b3cde4b108
navneshranjan <navneshranjan@gmail.com>
2021-07-19 16:35:35 +0530

    React- Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Amazon clone</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>CheckOut</h1>
+          </Route>
+          <Route path="/login">
+            <h1>LoginPage</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home Page!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
