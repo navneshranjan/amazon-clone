766bdfec32309f930dc6213451e636e678e43197
navneshranjan <navneshranjan@gmail.com>
2021-07-27 15:39:44 +0530

    [Banner update]

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home_image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/AmazonLaunchpad/Store/2021/Prime_day/PC_header3.jpg"
+        alt="Banner"
+      ></img>
+    </div>
+  );
+}
+
+export default Home;
